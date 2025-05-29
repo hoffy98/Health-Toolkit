fe74aa3389539859ac05c21dda63f45782594b54
hoffy98 <hofmannjohannes1998@gmail.com>
2025-05-29 16:08:40 +0200

    basic routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Layout from './Layout'
+import Home from './pages/Home'
+import Breath from './pages/Breath'
+import Train from './pages/Train'
+
 function App() {
   return (
-    <div className="w-svw h-svh flex justify-center items-center">
-      <h1>Health Toolkit</h1>
+    <div className="w-screen h-screen flex flex-col">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="breath" element={<Breath />} />
+            <Route path="train" element={<Train />} />
+          </Route>
+        </Routes>
+      </Router>
     </div>
   )
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+type Props = {}
+
+export default function Home({}: Props) {
+  return (
+    <div className="flex w-full h-full justify-center items-center">
+      <h1>Health-Toolkit</h1>
+    </div>
+  )
+}
